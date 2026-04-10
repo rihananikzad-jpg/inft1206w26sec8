89f9d754207ccd937d80f9b0ab899383b71dda2e
Riha <rihananikzad@gmail.com>
2026-04-10 19:56:50 -0400

    adding images.html

--- assignment4/part2/image.js
+++ assignment4/part2/image.js
@@ -3,3 +3,12 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// declare the images
+const images = [
+  { filename: 'pic1.jpg', alt: 'Closeup of a human eye' },
+  { filename: 'pic2.jpg', alt: 'Rock that looks like a wave' },
+  { filename: 'pic3.jpg', alt: 'White and purple pansies' },
+  { filename: 'pic4.jpg', alt: 'Section of wall from a pharaoh\'s tomb' },
+  { filename: 'pic5.jpg', alt: 'Large moth on a leaf' }
+];
