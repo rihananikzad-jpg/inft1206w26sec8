41770c7996461f7bb2d57312ace5679b81a63c6e
Riha <rihananikzad@gmail.com>
2026-04-10 20:01:10 -0400

    function.html

--- assignment4/part2/image.js
+++ assignment4/part2/image.js
@@ -12,3 +12,37 @@ const images = [
   { filename: 'pic4.jpg', alt: 'Section of wall from a pharaoh\'s tomb' },
   { filename: 'pic5.jpg', alt: 'Large moth on a leaf' }
 ];
+
+// loop
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', baseURL + image.filename);
+  newImage.setAttribute('alt', image.alt);
+
+  newImage.setAttribute('tabindex', '0');
+
+  thumbBar.appendChild(newImage);
+    // update image
+  newImage.addEventListener('click', () => {
+    updateDisplayedImage(newImage.getAttribute('src'), newImage.getAttribute('alt'));
+  });
+
+  // keyboard 
+
+  newImage.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      updateDisplayedImage(newImage.getAttribute('src'), newImage.getAttribute('alt'));
+    }
+  });
+}
+
+ // funcation
+
+function updateDisplayedImage(src, alt) {
+displayedImage.setAttribute('src', src);
+displayedImage.setAttribute('alt', alt);
+}
+
+
+
