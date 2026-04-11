15c9b38f2afc2b08adf69627c9c0a8e6f2f4f96c
Riha <rihananikzad@gmail.com>
2026-04-10 20:30:44 -0400

    check condtion.html

--- assignment4/part3/Object building.js
+++ assignment4/part3/Object building.js
@@ -57,3 +57,19 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  // check if the balles hitting eachother
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
