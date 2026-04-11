d0ba8f76e90d3280df09be69ac1f5f17ad553b46
Riha <rihananikzad@gmail.com>
2026-04-10 20:25:22 -0400

    adding the cnaves.html

--- assignment4/part3/Object building.js
+++ assignment4/part3/Object building.js
@@ -1,2 +1,8 @@
 // student name: raihana nikzad
 
+// canves
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
