0363acd69a55c347ee2a85c8e37494be78aae5cb
Riha <rihananikzad@gmail.com>
2026-04-10 20:59:40 -0400

    class.html

---
+++ assignment4/part4/main.js
@@ -0,0 +1,27 @@
+const para = document.querySelector('p');
+let count = 0;
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
+
+// shape class
+
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
