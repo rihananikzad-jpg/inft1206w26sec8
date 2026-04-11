20a911cc85972838ff9b2d5227e579614b586571
Riha <rihananikzad@gmail.com>
2026-04-10 20:26:02 -0400

    function.html

--- assignment4/part3/Object building.js
+++ assignment4/part3/Object building.js
@@ -6,3 +6,13 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// functions helpers
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
